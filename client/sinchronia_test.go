f9f9172b1c39294d1489405bb01ccd962eb7bda9
Akshay Narayan <akshayn@mit.edu>
2018-01-22 13:42:18 -0500

    test: add failing one-directional test

--- client/sinchronia_test.go
+++ client/sinchronia_test.go
@@ -14,31 +14,13 @@ import (
 
 // small-scale localhost test for sinchronia library and scheduler
 
-// steps
-// 1. start scheduler
-// 2. start application master
-func TestSinchronia(t *testing.T) {
-	done := make(chan interface{})
-	go func() {
-		c := make(chan os.Signal)
-		signal.Notify(c, syscall.SIGTERM)
-
-		<-c
-		done <- struct{}{}
-	}()
-	ready := make(chan interface{})
-	go runScheduler(context.Background(), ready, done)
-	<-ready
-	<-time.After(time.Millisecond)
-	appMaster("127.0.0.1:16424")
-	done <- struct{}{}
-}
-
 func runScheduler(
 	c context.Context,
 	ready chan interface{},
 	done chan interface{},
 ) {
+	killer := exec.Command("killall", "sinchronia-scheduler")
+	killer.Run()
 	ctx, cancel := context.WithCancel(c)
 	logF, err := os.Create("scheduler-test.log")
 	cmd := exec.CommandContext(ctx, "../scheduler/sinchronia-scheduler")
@@ -55,24 +37,21 @@ func runScheduler(
 	cancel()
 }
 
-// launch coflows for 4 clients
-// 1. coflows: [(1->2: 1MB, 3->4:1MB), (1->4:1MB, 2->4:1MB)]
-// 2. define coflows and register with scheduler
-// 3. start 4 clients
-func appMaster(schedAddr string) {
-	// define clients
-	// nodeID 0 = app master
-	nodes := map[uint32]string{
-		1: "127.0.0.1:17001",
-		2: "127.0.0.1:17002",
-		3: "127.0.0.1:17003",
-		4: "127.0.0.1:17004",
-	}
+// steps
+// 1. start scheduler
+// 2. start application master
+func TestSinchronia(t *testing.T) {
+	done := make(chan interface{})
+	go func() {
+		c := make(chan os.Signal)
+		signal.Notify(c, syscall.SIGTERM)
 
-	s, err := New(schedAddr, 0, nodes)
-	if err != nil {
-		panic(err)
-	}
+		<-c
+		done <- struct{}{}
+	}()
+	ready := make(chan interface{})
+	go runScheduler(context.Background(), ready, done)
+	<-ready
 
 	// define coflows
 	cf1 := Coflow{
@@ -127,33 +106,122 @@ func appMaster(schedAddr string) {
 		},
 	}
 
-	s.RegCoflow([]Coflow{cf1, cf2})
+	<-time.After(5 * time.Millisecond)
+	appMaster("127.0.0.1:16424", []Coflow{cf1, cf2})
+	done <- struct{}{}
+}
+
+func TestOneDirection(t *testing.T) {
+	done := make(chan interface{})
+	go func() {
+		c := make(chan os.Signal)
+		signal.Notify(c, syscall.SIGTERM)
+
+		<-c
+		done <- struct{}{}
+	}()
+	ready := make(chan interface{})
+	go runScheduler(context.Background(), ready, done)
+	<-ready
+
+	// define coflows
+	cf1 := Coflow{
+		JobID: 1,
+		Flows: []Flow{
+			Flow{
+				JobID: 1,
+				From:  1,
+				To:    2,
+				Info: Data{
+					DataID: 0,
+					Size:   5,
+					Blob:   []byte{'h', 'e', 'l', 'l', 'o'},
+				},
+			},
+			Flow{
+				JobID: 1,
+				From:  1,
+				To:    2,
+				Info: Data{
+					DataID: 1,
+					Size:   5,
+					Blob:   []byte{'h', 'e', 'l', 'l', 'o'},
+				},
+			},
+		},
+	}
+
+	cf2 := Coflow{
+		JobID: 2,
+		Flows: []Flow{
+			Flow{
+				JobID: 2,
+				From:  1,
+				To:    2,
+				Info: Data{
+					DataID: 2,
+					Size:   5,
+					Blob:   []byte{'h', 'e', 'l', 'l', 'o'},
+				},
+			},
+			Flow{
+				JobID: 2,
+				From:  1,
+				To:    2,
+				Info: Data{
+					DataID: 3,
+					Size:   5,
+					Blob:   []byte{'h', 'e', 'l', 'l', 'o'},
+				},
+			},
+		},
+	}
+	<-time.After(5 * time.Millisecond)
+	appMaster("127.0.0.1:16424", []Coflow{cf1, cf2})
+	done <- struct{}{}
+}
+
+func sliceForClient(cf []Coflow, client uint32) map[uint32][]Flow {
+	cfsl := make(map[uint32][]Flow)
+	for _, c := range cf {
+		cfsl[c.JobID] = make([]Flow, 0)
+		for _, f := range c.Flows {
+			if f.From == client {
+				cfsl[c.JobID] = append(cfsl[c.JobID], f)
+			}
+		}
+	}
+
+	return cfsl
+}
+
+// launch coflows for 4 clients
+// 1. coflows: [(1->2: 1MB, 3->4:1MB), (1->4:1MB, 2->4:1MB)]
+// 2. define coflows and register with scheduler
+// 3. start 4 clients
+func appMaster(schedAddr string, cfs []Coflow) {
+	// define clients
+	// nodeID 0 = app master
+	nodes := map[uint32]string{
+		1: "127.0.0.1:17001",
+		2: "127.0.0.1:17002",
+		3: "127.0.0.1:17003",
+		4: "127.0.0.1:17004",
+	}
+
+	s, err := New(schedAddr, 0, nodes)
+	if err != nil {
+		panic(err)
+	}
+
+	s.RegCoflow(cfs)
 
 	done := make(chan interface{})
 
-	c1sl := map[uint32][]Flow{
-		1: []Flow{cf1.Flows[0]},
-		2: []Flow{cf2.Flows[0]},
-	}
-	go client(schedAddr, 1, nodes, c1sl, done)
-
-	c2sl := map[uint32][]Flow{
-		1: []Flow{},
-		2: []Flow{cf2.Flows[1]},
-	}
-	go client(schedAddr, 2, nodes, c2sl, done)
-
-	c3sl := map[uint32][]Flow{
-		1: []Flow{cf1.Flows[1]},
-		2: []Flow{},
-	}
-	go client(schedAddr, 3, nodes, c3sl, done)
-
-	c4sl := map[uint32][]Flow{
-		1: []Flow{},
-		2: []Flow{},
-	}
-	go client(schedAddr, 4, nodes, c4sl, done)
+	go client(schedAddr, 1, nodes, sliceForClient(cfs, 1), done)
+	go client(schedAddr, 2, nodes, sliceForClient(cfs, 2), done)
+	go client(schedAddr, 3, nodes, sliceForClient(cfs, 3), done)
+	go client(schedAddr, 4, nodes, sliceForClient(cfs, 4), done)
 
 	<-done
 	<-done
